c30d826164e1c1ce7d20028008419fef580aee34
Lucas Marques <marquesfaria99@gmail.com>
2025-05-09 18:49:06 -0300

    feat: migrations criadas

---
+++ database/migrations/2025_05_09_214710_create_service_orders_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('service_orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['pendente', 'em_andamento', 'concluida'])->default('pendente');
+            $table->foreignId('client_id')->constrained('users')->onUpdate('cascade')->onDelete('restrict');
+            $table->foreignId('technician_id')->constrained('users')->onUpdate('cascade')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('service_orders');
+    }
+};
